c8b7360a421fec11e51be1a0e4ad044bb7a71036
Sanchita Mahimkar <sanchi.b.mahimkar@gmail.com>
2024-12-11 11:50:30 +0530

    Add project files.

---
+++ OnlineCourseRegistration/ViewModel/StudentViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace OnlineCourseRegistration.ViewModel
+{
+    public class StudentViewModel
+    {
+        public string FName { get; set; }
+        public string MName { get; set; }
+        public string LName { get; set; }
+        public DateTime DOB { get; set; }
+        public string gender { get; set; }
+        public string address { get; set; }           
+        public string city { get; set; }
+        public string country { get; set; }
+        public string email { get; set; }
+        public int contactno { get; set; }
+        public string course { get; set; }
+    }
+}
